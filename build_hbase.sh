ad58da9d4f7d035ad49641dda42769c24bdd4d63
Minwoo Kang <its.minwoo.kang@gmail.com>
2022-03-25 14:07:33 +0900

    add build_hbase.sh

---
+++ build_hbase.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export MAVEN_OPTS="-Xms4g -Xmx4g"
+
+mvn clean package -DskipTests -Drat.skip=true -Drat.ignoreErrors=true -Dcheckstyle.skip=true  -Dhadoop.profile=3.0 -Dadditionalparam=-Xdoclint:none assembly:single
