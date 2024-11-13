b07bab436ebea7d4e9e8a6df52ef99dd7de6b761
Minwoo Kang <minwoo.kang@navercorp.com>
2024-11-13 20:42:35 +0900

    Add reproduction code for countOfRowsFiltered

---
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/regionserver/TestRowsFilteredMetrics.java
@@ -0,0 +1,69 @@
+package org.apache.hadoop.hbase.regionserver;
+
+import java.io.IOException;
+
+import org.apache.hadoop.hbase.HBaseTestingUtil;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.Delete;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.client.ResultScanner;
+import org.apache.hadoop.hbase.client.Scan;
+import org.apache.hadoop.hbase.client.Table;
+import org.apache.hadoop.hbase.client.metrics.ScanMetrics;
+import org.apache.hadoop.hbase.testclassification.LargeTests;
+import org.apache.hadoop.hbase.util.Bytes;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+@Category(LargeTests.class)
+public class TestRowsFilteredMetrics {
+  private final static HBaseTestingUtil TEST_UTIL = new HBaseTestingUtil();
+
+  private static TableName TABLE_NAME = TableName.valueOf("testRowsFilteredMetricsTable");
+  private static byte[] FAMILY = Bytes.toBytes("testFamily");
+
+  private static Table table;
+
+  @BeforeClass
+  public static void setUpBeforeClass() throws Exception {
+    TEST_UTIL.startMiniCluster(1);
+    table = TEST_UTIL.createTable(TABLE_NAME, FAMILY);
+  }
+
+  @AfterClass
+  public static void tearDownAfterClass() throws Exception {
+    TEST_UTIL.shutdownMiniCluster();
+  }
+
+  @Test
+  public void testRowsFilteredMetrics() throws Exception {
+    // put
+    Put put = new Put(Bytes.toBytes("row1"));
+    put.addColumn(FAMILY, Bytes.toBytes("qual"), Bytes.toBytes("value"));
+    table.put(put);
+
+    // scan
+    scan();
+
+    // delete
+    Delete delete = new Delete(Bytes.toBytes("row1"));
+    table.delete(delete);
+
+    scan();
+  }
+
+  private static void scan() throws IOException {
+    final Scan scan = new Scan();
+    scan.setScanMetricsEnabled(true);
+    final ResultScanner resultScanner = table.getScanner(scan);
+
+    resultScanner.forEach(result -> {
+    });
+
+    final ScanMetrics metrics = resultScanner.getScanMetrics();
+    System.out.printf("countOfRowsScanned: %s, countOfRowsFiltered: %s\n",
+        metrics.countOfRowsScanned, metrics.countOfRowsFiltered);
+  }
+}
